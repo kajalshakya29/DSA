1beb0141e9a5bf8b77f8d551f5fefe82276e2501
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2025-07-30 05:56:24 +0530

    Add files via upload

---
+++ Recursion/GreatestCommonDivisor/src/GreatestCommonDivisor.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class GreatestCommonDivisor {
+    public static int gcd(int a, int b){
+        if (b%a==0) return a;
+        return gcd(b%a,a);
+    }
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a:");
+        int a =sc.nextInt();
+        System.out.println("Enter b:");
+        int b =sc.nextInt();
+        System.out.println(gcd(a,b));
+    }
+}
