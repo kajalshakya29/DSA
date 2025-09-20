3b20e043858bdea5de391a782a979b005ded8b4e
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2025-09-20 17:24:27 +0530

    Add files via upload

---
+++ Stacks/Copy_Stack_Into_Another/src/Main.java
@@ -0,0 +1,35 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Stack<Integer> st=new Stack<>();
+        //System.out.println(st.isEmpty());
+        st.push(1);
+        st.push(5);
+        st.push(10);
+        st.push(20);
+        System.out.println(st);
+
+        Stack<Integer>st_reverse=new Stack<>();
+        while(!st.isEmpty()){
+            //int ele = st.pop();
+            //st_reverse.push(ele);
+
+            //-----OR-----
+            st_reverse.push(st.pop());  //-----TC=O(N) ----SC=O(N)
+        }
+        System.out.println(st_reverse);
+
+        Stack<Integer>st_Copy=new Stack<>();
+        while(!st_reverse.isEmpty()){
+            //int ele = st.pop();
+            //st_reverse.push(ele);
+
+            //-----OR-----
+            st_Copy.push(st_reverse.pop());  //-----TC=O(N) ----SC=O(N)
+        }
+        System.out.println(st_Copy );
+
+    }
+}
