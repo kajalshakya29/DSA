a1114dc33fceadeeaf74aadc48dfa56bf9a391dd
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2026-03-07 12:33:53 +0530

    Add files via upload

---
+++ Stacks/Stack_Underflow/src/Main.java
@@ -0,0 +1,26 @@
+import java.util.Stack;
+
+public class Main {
+    public static void main(String[] args) {
+        Stack<Integer> st=new Stack<>();
+        st.push(1);
+        st.push(2);
+        st.push(3);
+        st.push(4);
+        st.push(5);
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.pop();
+        System.out.println(st);
+        st.pop(); //-->Overflow condition
+        System.out.println(st);
+
+    }
+}
