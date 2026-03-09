3f79f71d3cb8d3511951e9678c6dbac00465e2d3
kajal shakya <157579940+kajalshakya29@users.noreply.github.com>
2026-03-09 09:09:32 +0530

    Add files via upload

---
+++ Arrays/NextGreatestElement.java
@@ -0,0 +1,29 @@
+package Practice;
+
+public class NextGreatestElement {
+    public static void main(String[] args) {
+        int[] arr={21,43,60,12,10,59,50};
+        int n= arr.length;
+        int[] ans=new int[n];
+        ans[n-1]=-1;
+        int nge=arr[n-1];
+        for (int i = n-2; i >=0 ; i--) {
+            ans[i]=nge;
+            nge=Math.max(nge,arr[i]);
+        }
+        /* for (int i = 0; i < n-1; i++) {
+            int mx=Integer.MIN_VALUE;
+            for (int j = i+1; j <n ; j++) {
+                mx=Math.max(mx,arr[j]);
+            }
+            ans[i]=mx;
+        }*/
+        for (int ele:arr){
+            System.out.print(ele+ " ");
+        }
+        System.out.println();
+        for (int ele:ans){
+            System.out.print(ele+ " ");
+        }
+    }
+}
